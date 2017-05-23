fdbe0267774b1e5501cb6c335b0ce0bce4b66a36
rafikurnia <rafi.kurnia.putra@gmail.com>
2017-05-23 13:34:54 +0700

    added header

---
+++ scrap/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2017 Rafi Kurnia Putra
+# Licensed under the MIT - https://opensource.org/licenses/MIT
+
+"""
+scrap - Created by Rafi Kurnia Putra <rafi.kurnia.putra@gmail.com> on 23/05/2017
+"""
+
