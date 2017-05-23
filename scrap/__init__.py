b6cbfafa38440c4d910a6cc7b054541ddca8c36a
rafikurnia <rafi.kurnia.putra@gmail.com>
2017-05-23 13:40:24 +0700

    added class

--- scrap/__init__.py
+++ scrap/__init__.py
@@ -8,3 +8,19 @@
 scrap - Created by Rafi Kurnia Putra <rafi.kurnia.putra@gmail.com> on 23/05/2017
 """
 
+
+class Scrap(object):
+    """
+    Scrap facebook user's posts and comments
+    """
+
+    def __init__(self, facebook_user_url):
+        self.facebook_user_url = facebook_user_url
+
+    def scrap(self):
+        """
+        Start scrapping on the user page
+        :return: list of contents
+        """
+
+        return [self.facebook_user_url]
